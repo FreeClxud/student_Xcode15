a1fe5cfdd6c59ac127774a7ceafb8cdb8ca301de
FreeClxud <Romerom6470@durhamtech.edu>
2024-10-27 15:48:55 -0400

    Update Contents.swift

--- Unit1/Lesson1_2/Lab_Introduction.playground/Contents.swift
+++ Unit1/Lesson1_2/Lab_Introduction.playground/Contents.swift
@@ -12,11 +12,15 @@ print("How to use playgrounds to make writing Swift fun and simple")
 /*:
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
+print("Song Title: Long Time")
+print("Artist: Playboy Carti")
 
 
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.
  */
-
+print("I been waiting for a long time")
+print("Living life like it's a dream")
+print("Everything ain't what it seems")
 
 //:page 14 of 16  |  [Next: Exercise: Go! Fight! Win!](@next)
