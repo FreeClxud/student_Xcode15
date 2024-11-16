5de859c7227673b98ffc298bf33627d54b203e3c
FreeClxud <Romerom6470@durhamtech.edu>
2024-11-16 13:25:57 -0500

    Update Contents.swift

--- Unit2/Lesson2_3/Lab_Functions.playground/Pages/4. App Exercise - Progress Updates.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_3/Lab_Functions.playground/Pages/4. App Exercise - Progress Updates.xcplaygroundpage/Contents.swift
@@ -8,11 +8,41 @@
  Rewrite the function `progressUpdate`, only this time give it two parameters of type `Int` called `steps` and `goal`, respectively. Like before, it should print "You're off to a good start." if steps is less than 10% of goal, "You're almost halfway there!" if steps is less than half of goal, "You're over halfway there!" if steps is less than 90% of goal, "You're almost there!" if steps is less than goal, and "You beat your goal!" otherwise. Call the function and observe the printout.
  
  Call the function a number of times, passing in different values of `steps` and `goal`. Observe the printouts and make sure what is printed to the console is what you would expect for the parameters passsed in.
- */
+ */func progressUpdate(steps: Int, goal: Int) {
+    if steps < goal * 0.1 {
+        print("You're off to a good start.")
+    } else if steps < goal / 2 {
+        print("You're almost halfway there!")
+    } else if steps < goal * 0.9 {
+        print("You're over halfway there!")
+    } else if steps < goal {
+        print("You're almost there!")
+    } else {
+        print("You beat your goal!")
+    }
+}
+
+progressUpdate(5, goal: 50)
+progressUpdate(20, goal: 50)
+progressUpdate(30, goal: 50)
+progressUpdate(45, goal: 50)
+progressUpdate(50, goal: 50)
 
 
 //:  Your fitness tracking app is going to help runners stay on pace to reach their goals. Write a function called pacing that takes four `Double` parameters called `currentDistance`, `totalDistance`, `currentTime`, and `goalTime`. Your function should calculate whether or not the user is on pace to hit or beat `goalTime`. If yes, print "Keep it up!", otherwise print "You've got to push it just a bit harder!"
+func pacing(currentDistance: Double, totalDistance: Double, currentTime: Double, goalTime: Double) {
+    let pace = currentTime / currentDistance
+    let expectedTime = pace * totalDistance
+    
+    if expectedTime <= goalTime {
+        print("Keep it up!")
+    } else {
+        print("You've got to push it just a bit harder!")
+    }
+}
 
+pacing(currentDistance: 5.0, totalDistance: 10.0, currentTime: 30.0, goalTime: 60.0)
+pacing(currentDistance: 5.0, totalDistance: 10.0, currentTime: 35.0, goalTime: 60.0)
 
 /*:
 [Previous](@previous)  |  page 4 of 6  |  [Next: Exercise - Return Values](@next)
