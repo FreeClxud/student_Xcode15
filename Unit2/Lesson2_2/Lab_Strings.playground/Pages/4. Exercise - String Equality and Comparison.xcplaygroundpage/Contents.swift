fa8e3d7716ff87fa29d88683b02f23c968d37cd1
FreeClxud <Romerom6470@durhamtech.edu>
2024-11-17 11:47:19 -0500

    Update Contents.swift

--- Unit2/Lesson2_2/Lab_Strings.playground/Pages/4. Exercise - String Equality and Comparison.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_2/Lab_Strings.playground/Pages/4. Exercise - String Equality and Comparison.xcplaygroundpage/Contents.swift
@@ -2,7 +2,7 @@
 ## Exercise - String Equality and Comparison
  
  Create two constants, `nameInCaps` and `name`. Assign `nameInCaps` your name as a string literal with proper capitalization. Assign `name` your name as a string literal in all lowercase. Write an if-else statement that checks to see if `nameInCaps` and `name` are the same. If they are, print "The two strings are equal," otherwise print "The two strings are not equal."
- */let nameInCaps = "Manuel"
+ * let nameInCaps = "Manuel"
 let name = "manuel"
 
 if nameInCaps == name {
@@ -10,6 +10,7 @@ if nameInCaps == name {
 } else {
     print("The two strings are not equal")
 }
+
 
 
 
@@ -21,7 +22,7 @@ if nameInCaps == name {
  If they are not equal, print the following statement using string interpolation:
  
 - "<INSERT LOWERCASED VERSION OF `nameInCaps` HERE> and <INSERT LOWERCASED VERSION OF `name` HERE> are not the same."
- */let nameInCaps = "Manuel"
+ * let nameInCaps = "Manuel"
 let name = "manuel"
 
 if nameInCaps.lowercased() == name.lowercased() {
@@ -29,6 +30,7 @@ if nameInCaps.lowercased() == name.lowercased() {
 } else {
     print("\(nameInCaps.lowercased()) and \(name.lowercased()) are not the same.")
 }
+
 
 
 
@@ -44,6 +46,7 @@ if junior.hasSuffix("Jr.") {
 
 //:  Suppose you are trying to find a document on your computer that contains Hamlet's famous soliloquy written by Shakespeare. You write a simple app that will check every document to see if it contains the phrase "to be, or not to be." You decide to do part of this with the `contains(_:)` method. Write an if statement below that will check if `textToSearchThrough` contains `textToSearchFor`. If it does, print "I found it!" Be sure to make this functionality case insensitive.
 import Foundation
+
 let textToSearchThrough = "To be, or not to be--that is the question"
 let textToSearchFor = "to be, or not to be"
 
@@ -52,10 +55,10 @@ if textToSearchThrough.lowercased().contains(textToSearchFor.lowercased()) {
 }
 
 
+
 //:  Print to the console the number of characters in your name by using the `count` property on `name`.
 let name = "Manuel"
 print(name.count)
-
 
 /*:
 [Previous](@previous)  |  page 4 of 5  |  [Next: App Exercise - Password Entry and User Search](@next)
